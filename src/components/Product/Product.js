53a78ca1f22330729338b8971605f917854b7137
osmnc <osmn.cn87@hotmail.com>
2022-06-13 21:08:25 +0300

    Product component created.

---
+++ src/components/Product/Product.js
@@ -0,0 +1,202 @@
+import React from "react";
+import "./_product.scss";
+
+function Product() {
+  return (
+    <>
+      <div className="container product-container">
+        <div className="selected-category">
+          <img
+            src="/assets/images/selected_category_icon.svg"
+            alt="selected_category"
+          />
+          Tüm Kategoriler
+        </div>
+        <div className="products">
+          <div className="product">
+            <img src="/assets/images/watch.png" alt="product" />
+            <div className="product-text">
+              Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth
+            </div>
+            <div className="delivery-info">Ücretsiz Teslimat</div>
+            <div className="product-price">399,90 TL</div>
+            <button>Sepete Ekle</button>
+            {/* <div className="sepet-counter">
+              <div className="counter-decrement">
+                <img src="/assets/images/minus-sign.png" alt="minus_icon" />
+              </div>
+              <div className="count">2</div>
+              <div className="counter-increment">
+                <img src="/assets/images/add-button.png" alt="plus_icon" />
+              </div>
+            </div> */}
+          </div>
+          <div className="product">
+            <img src="/assets/images/watch.png" alt="product" />
+            <div className="product-text">
+              Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth
+            </div>
+            <div className="delivery-info">Ücretsiz Teslimat</div>
+            <div className="product-price">399,90 TL</div>
+            {/* <button>Sepete Ekle</button> */}
+            <div className="sepet-counter">
+              <div className="counter-decrement">
+                <img src="/assets/images/minus-sign.png" alt="minus_icon" />
+              </div>
+              <div className="count">2</div>
+              <div className="counter-increment">
+                <img src="/assets/images/add-button.png" alt="plus_icon" />
+              </div>
+            </div>
+          </div>
+          <div className="product">
+            <img src="/assets/images/watch.png" alt="product" />
+            <div className="product-text">
+              Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth
+            </div>
+            <div className="delivery-info">Ücretsiz Teslimat</div>
+            <div className="product-price">399,90 TL</div>
+            {/* <button>Sepete Ekle</button> */}
+            <div className="sepet-counter">
+              <div className="counter-decrement">
+                <img src="/assets/images/minus-sign.png" alt="minus_icon" />
+              </div>
+              <div className="count">2</div>
+              <div className="counter-increment">
+                <img src="/assets/images/add-button.png" alt="plus_icon" />
+              </div>
+            </div>
+          </div>
+          <div className="product">
+            <img src="/assets/images/watch.png" alt="product" />
+            <div className="product-text">
+              Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth
+            </div>
+            <div className="delivery-info">Ücretsiz Teslimat</div>
+            <div className="product-price">399,90 TL</div>
+            {/* <button>Sepete Ekle</button> */}
+            <div className="sepet-counter">
+              <div className="counter-decrement">
+                <img src="/assets/images/minus-sign.png" alt="minus_icon" />
+              </div>
+              <div className="count">2</div>
+              <div className="counter-increment">
+                <img src="/assets/images/add-button.png" alt="plus_icon" />
+              </div>
+            </div>
+          </div>
+          <div className="product">
+            <img src="/assets/images/watch.png" alt="product" />
+            <div className="product-text">
+              Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth
+            </div>
+            <div className="delivery-info">Ücretsiz Teslimat</div>
+            <div className="product-price">399,90 TL</div>
+            {/* <button>Sepete Ekle</button> */}
+            <div className="sepet-counter">
+              <div className="counter-decrement">
+                <img src="/assets/images/minus-sign.png" alt="minus_icon" />
+              </div>
+              <div className="count">2</div>
+              <div className="counter-increment">
+                <img src="/assets/images/add-button.png" alt="plus_icon" />
+              </div>
+            </div>
+          </div>
+          <div className="product">
+            <img src="/assets/images/watch.png" alt="product" />
+            <div className="product-text">
+              Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth
+            </div>
+            <div className="delivery-info">Ücretsiz Teslimat</div>
+            <div className="product-price">399,90 TL</div>
+            {/* <button>Sepete Ekle</button> */}
+            <div className="sepet-counter">
+              <div className="counter-decrement">
+                <img src="/assets/images/minus-sign.png" alt="minus_icon" />
+              </div>
+              <div className="count">2</div>
+              <div className="counter-increment">
+                <img src="/assets/images/add-button.png" alt="plus_icon" />
+              </div>
+            </div>
+          </div>
+          <div className="product">
+            <img src="/assets/images/watch.png" alt="product" />
+            <div className="product-text">
+              Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth
+            </div>
+            <div className="delivery-info">Ücretsiz Teslimat</div>
+            <div className="product-price">399,90 TL</div>
+            {/* <button>Sepete Ekle</button> */}
+            <div className="sepet-counter">
+              <div className="counter-decrement">
+                <img src="/assets/images/minus-sign.png" alt="minus_icon" />
+              </div>
+              <div className="count">2</div>
+              <div className="counter-increment">
+                <img src="/assets/images/add-button.png" alt="plus_icon" />
+              </div>
+            </div>
+          </div>
+          <div className="product">
+            <img src="/assets/images/watch.png" alt="product" />
+            <div className="product-text">
+              Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth
+            </div>
+            <div className="delivery-info">Ücretsiz Teslimat</div>
+            <div className="product-price">399,90 TL</div>
+            {/* <button>Sepete Ekle</button> */}
+            <div className="sepet-counter">
+              <div className="counter-decrement">
+                <img src="/assets/images/minus-sign.png" alt="minus_icon" />
+              </div>
+              <div className="count">2</div>
+              <div className="counter-increment">
+                <img src="/assets/images/add-button.png" alt="plus_icon" />
+              </div>
+            </div>
+          </div>
+          <div className="product">
+            <img src="/assets/images/watch.png" alt="product" />
+            <div className="product-text">
+              Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth
+            </div>
+            <div className="delivery-info">Ücretsiz Teslimat</div>
+            <div className="product-price">399,90 TL</div>
+            {/* <button>Sepete Ekle</button> */}
+            <div className="sepet-counter">
+              <div className="counter-decrement">
+                <img src="/assets/images/minus-sign.png" alt="minus_icon" />
+              </div>
+              <div className="count">2</div>
+              <div className="counter-increment">
+                <img src="/assets/images/add-button.png" alt="plus_icon" />
+              </div>
+            </div>
+          </div>
+          <div className="product">
+            <img src="/assets/images/watch.png" alt="product" />
+            <div className="product-text">
+              Z6 Akıllı Saat Kameralı Konuşma Garantili Sim Kartlı Bluetooth
+            </div>
+            <div className="delivery-info">Ücretsiz Teslimat</div>
+            <div className="product-price">399,90 TL</div>
+            {/* <button>Sepete Ekle</button> */}
+            <div className="sepet-counter">
+              <div className="counter-decrement">
+                <img src="/assets/images/minus-sign.png" alt="minus_icon" />
+              </div>
+              <div className="count">2</div>
+              <div className="counter-increment">
+                <img src="/assets/images/add-button.png" alt="plus_icon" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Product;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Category from "../../components/Category/Category";
 import Navbar from "../../components/Navbar/Navbar";
+import Product from "../../components/Product/Product";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Category />
+      <Product />
     </>
   );
 }
